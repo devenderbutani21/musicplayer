d7ad420277db3a97e05485c6c352c1556178e4b9
Devender Butani <devenderbutani21@gmail.com>
2024-06-04 11:20:06 -0400

    Logo Screen Built

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,22 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
+import 'package:musicapp/screens/logo_screen.dart';
+import 'package:musicapp/screens/music_screen.dart';
+import 'package:path_provider/path_provider.dart';
+
 
 void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
+  runApp(
+    MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Music App',
       theme: ThemeData(
-        primarySwatch: Colors.deepOrange,
+        primaryColor: Colors.orange,
       ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Music App'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Your Music Library',
-            ),
-          ],
-        ),
-      ),
-    );
-  }
+      routes: {
+        "/" : (context) => const LogoScreen(),
+        "/music" : (context) => const MusicScreen(),
+        "/logo" : (context) => const LogoScreen(),
+      },
+    ),
+  );
 }
---
+++ lib/screens/logo_screen.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+
+class LogoScreen extends StatelessWidget {
+  const LogoScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                Colors.black54,
+                Colors.black87,
+              ]),
+        ),
+        child: Column(
+          children: <Widget>[
+            const SizedBox(
+              height: 180,
+            ),
+            Center(
+              child: Image.asset(
+                "assets/images/logo.png",
+                height: 320,
+                width: 320,
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const Text(
+              "Listen to Your",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            const Text(
+              "Favorite Music",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 32,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Stack(
+              children: <Widget>[
+                const Center(
+                  child: SpinKitRipple(
+                    color: Colors.orange,
+                    size: 180,
+                  ),
+                ),
+                Column(
+                  children: [
+                    const SizedBox(
+                      height: 40,
+                    ),
+                    Center(
+                      child: Image.asset(
+                        "assets/images/play.png",
+                        height: 100,
+                        width: 100,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

2665d1f3a1b9168ec20cc1c6cdb63bf70577339b
Devender Butani <devenderbutani21@gmail.com>
2024-06-04 13:13:51 -0400

    Music Screen Changes

--- lib/screens/logo_screen.dart
+++ lib/screens/logo_screen.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:musicapp/screens/music_screen.dart';
 
 class LogoScreen extends StatelessWidget {
   const LogoScreen({super.key});
@@ -7,80 +10,92 @@ class LogoScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: [
-                Colors.black54,
-                Colors.black87,
-              ]),
+        body: FutureBuilder(
+      future: Future.delayed(
+        const Duration(
+          seconds: 4,
         ),
-        child: Column(
-          children: <Widget>[
-            const SizedBox(
-              height: 180,
+      ),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MusicScreen();
+        } else {
+          return Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Colors.black54,
+                    Colors.black87,
+                  ]),
             ),
-            Center(
-              child: Image.asset(
-                "assets/images/logo.png",
-                height: 320,
-                width: 320,
-              ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            const Text(
-              "Listen to Your",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 28,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
-            const Text(
-              "Favorite Music",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 32,
-                fontWeight: FontWeight.w500,
-              ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            Stack(
+            child: Column(
               children: <Widget>[
-                const Center(
-                  child: SpinKitRipple(
-                    color: Colors.orange,
-                    size: 180,
+                const SizedBox(
+                  height: 180,
+                ),
+                Center(
+                  child: Image.asset(
+                    "assets/images/logo.png",
+                    height: 320,
+                    width: 320,
                   ),
                 ),
-                Column(
-                  children: [
-                    const SizedBox(
-                      height: 40,
-                    ),
-                    Center(
-                      child: Image.asset(
-                        "assets/images/play.png",
-                        height: 100,
-                        width: 100,
+                const SizedBox(
+                  height: 20,
+                ),
+                const Text(
+                  "Listen to Your",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 28,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const Text(
+                  "Favorite Music",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 32,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Stack(
+                  children: <Widget>[
+                    const Center(
+                      child: SpinKitRipple(
+                        color: Colors.orange,
+                        size: 180,
                       ),
+                    ),
+                    Column(
+                      children: [
+                        const SizedBox(
+                          height: 40,
+                        ),
+                        Center(
+                          child: Image.asset(
+                            "assets/images/play.png",
+                            height: 100,
+                            width: 100,
+                          ),
+                        ),
+                      ],
                     ),
                   ],
                 ),
               ],
             ),
-          ],
-        ),
-      ),
-    );
+          );
+        }
+      },
+    ));
   }
 }
--- lib/screens/music_screen.dart
+++ lib/screens/music_screen.dart
@@ -1,10 +1,85 @@
 import 'package:flutter/material.dart';
 
-class MusicScreen extends StatelessWidget {
+class MusicScreen extends StatefulWidget {
   const MusicScreen({super.key});
 
   @override
+  State<MusicScreen> createState() => _MusicScreenState();
+}
+
+class _MusicScreenState extends State<MusicScreen> {
+  String searchText = "";
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Colors.black54,
+              Colors.black87,
+            ],
+          ),
+        ),
+        child: SafeArea(
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 20,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Align(
+                  alignment: Alignment.topCenter,
+                  child: Container(
+                    height: 50,
+                    decoration: BoxDecoration(
+                      color: Colors.black45,
+                      borderRadius: BorderRadius.circular(25),
+                    ),
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    child: TextField(
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                      onChanged: (value) {
+                        setState(() {
+                          searchText = value;
+                        });
+                      },
+                      decoration: InputDecoration(
+                        hintText: "Search...",
+                        hintStyle: TextStyle(
+                          color: Colors.white54,
+                        ),
+                        border: InputBorder.none,
+                        prefixIcon: const Icon(
+                          Icons.search,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              // Rest of your content here
+              Expanded(
+                child: Center(
+                  child: Text(
+                    "Future Music!",
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
